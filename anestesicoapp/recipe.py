687061b369d570deee6bb9dcaae98ff108ad4ee0
alexaraujodelima <alexaraujodlima@icloud.com>
2024-11-06 00:54:58 -0300

    Add files via upload

--- anestesicoapp/recipe.py
+++ anestesicoapp/recipe.py
@@ -2,10 +2,8 @@ from kivy_ios.toolchain import PythonRecipe
 
 class AnestesicoAppRecipe(PythonRecipe):
     version = "1.0"
+    url = "https://github.com/alexaraujodelima/anestesicoapp/archive/refs/heads/main.zip"
     depends = ["kivy"]
     site_packages_name = "anestesicoapp"
-    
-    # Adicione a variável url
-    url = "https://github.com/username/anestesicoapp/archive/refs/heads/main.zip"  # Substitua pela URL correta do seu repositório
 
 recipe = AnestesicoAppRecipe()
